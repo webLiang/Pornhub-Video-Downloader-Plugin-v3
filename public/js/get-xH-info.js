1fed2dc90b744b9ccb27f7041916a576e2d619c5
Leon <Leon@example.com>
2024-05-20 20:48:24 +0800

    feat(blog): support xhamster.com && speed optimizing

---
+++ public/js/get-xH-info.js
@@ -0,0 +1,10 @@
+(function () {
+  const mp4Urls = window.initials.xplayerSettings.sources.standard.h264;
+  console.log('🚀 ~ mp4Urls:', mp4Urls);
+  window.postMessage({
+    type: 'main-get-xv-info',
+    data: mp4Urls.map(item => {
+      return { videoUrl: item.url, quality: item.quality, format: 'mp4' };
+    }),
+  });
+})();
